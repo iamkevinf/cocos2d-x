4bde61038807c2a7f40c9f3ba472ff186a7f9801
youlanhai <you_lan_hai@foxmail.com>
2014-02-20 16:41:29 +0800

    vertex buffer

---
+++ cocos/3d/VertexBuffer.cpp
@@ -0,0 +1,137 @@
+//
+//  VertexBuffer.cpp
+//  cocos3d_libs
+//
+//  Created by zhoubao on 14-2-20.
+//  Copyright (c) 2014年 zhoubao. All rights reserved.
+//
+
+#include "VertexBuffer.h"
+
+namespace my3d
+{
+
+    GLenum bufferUsage2GL(BufferUsage usage)
+    {
+        switch(usage)
+        {
+            case BufferUsage::Dynamic:
+                return GL_DYNAMIC_DRAW;
+                
+            default:
+                return GL_STATIC_DRAW;
+        };
+    }
+
+    GLenum bufferType2GL(BufferType type)
+    {
+        switch(type)
+        {
+            case BufferType::Vertex:
+                return GL_ARRAY_BUFFER;
+                
+            case BufferType::Index:
+                return GL_ELEMENT_ARRAY_BUFFER;
+                
+            default:
+                assert("unsupported buffer type!");
+                return -1;
+        };
+    }
+
+    //////////////////////////////////////////////////////////////////////
+
+    BufferBase::BufferBase(BufferType type, BufferUsage usage)
+    : m_type(type)
+    , m_usage(usage)
+    , m_size(0)
+    , m_vb(0)
+    {
+        init();
+    }
+
+    BufferBase::~BufferBase()
+    {
+        destroy();
+    }
+
+    bool BufferBase::isValid() const
+    {
+        return m_vb != 0;
+    }
+
+    void BufferBase::resize(size_t size, void *data)
+    {
+        if(isValid())
+        {
+            bind();
+            glBufferData(bufferType2GL(m_type), size, data, bufferUsage2GL(m_usage));
+            unbind();
+            
+            m_size = size;
+        }
+    }
+
+    void BufferBase::setVertex(size_t offset, size_t size, void *data)
+    {
+        bind();
+        glBufferSubData(bufferType2GL(m_type), offset, size, data);
+        unbind();
+    }
+
+
+    bool BufferBase::init()
+    {
+        GLuint buffers[1] = {0};
+        glGenBuffers(1, buffers);
+        m_vb = buffers[0];
+        return m_vb != 0;
+    }
+
+    void BufferBase::bind()
+    {
+        glBindBuffer(bufferType2GL(m_type), m_vb);
+    }
+
+    void BufferBase::unbind()
+    {
+        glBindBuffer(bufferType2GL(m_type), 0);
+    }
+
+    void BufferBase::destroy()
+    {
+        if(isValid())
+        {
+            GLuint buffers[1] = {m_vb};
+            glDeleteBuffers(1, buffers);
+            m_vb = 0;
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////
+
+    VertexBuffer::VertexBuffer(BufferUsage usage, size_t size, void *data)
+    : BufferBase(BufferType::Vertex, usage)
+    {
+        resize(size, data);
+    }
+
+    VertexBuffer::~VertexBuffer()
+    {
+    }
+
+    //////////////////////////////////////////////////////////////////////
+
+    IndexBuffer::IndexBuffer(BufferUsage usage, size_t size, void *data)
+    : BufferBase(BufferType::Index, usage)
+    {
+        resize(size, data);
+    }
+
+    IndexBuffer::~IndexBuffer()
+    {
+        
+    }
+        
+
+}//end namespace my3d
---
+++ cocos/3d/VertexBuffer.h
@@ -0,0 +1,82 @@
+//
+//  VertexBuffer.h
+//  cocos3d_libs
+//
+//  Created by zhoubao on 14-2-20.
+//  Copyright (c) 2014年 zhoubao. All rights reserved.
+//
+
+#ifndef __cocos3d_libs__VertexBuffer__
+#define __cocos3d_libs__VertexBuffer__
+
+#include "EnumDef.h"
+#include "cocos2d.h"
+
+namespace my3d
+{
+
+    enum BufferType
+    {
+        Vertex = 0,
+        Index = 1
+    };
+
+    enum BufferUsage
+    {
+        Static = 0,
+        Dynamic = 1
+    };
+
+    class BufferBase : public cocos2d::Object
+    {
+        BufferBase(const BufferBase &);
+        const BufferBase & operator = (const BufferBase &);
+        
+    public:
+        
+        BufferBase(BufferType type, BufferUsage usage);
+        virtual ~BufferBase();
+        
+        bool isValid() const;
+        
+        void resize(size_t size, void *data = nullptr);
+        void setVertex(size_t offset, size_t size, void *data);
+        
+        void bind();
+        void unbind();
+        
+    private:
+        
+        bool init();
+        void destroy();
+        
+    private:
+        BufferType  m_type;
+        BufferUsage m_usage;
+        size_t m_size;
+        GLuint m_vb;
+    };
+
+    //顶点缓冲区
+    class VertexBuffer : public BufferBase
+    {
+    public:
+        
+        VertexBuffer(BufferUsage usage, size_t size, void *data=nullptr);
+        ~VertexBuffer();
+        
+    };
+
+    //索引缓冲区
+    class IndexBuffer : public BufferBase
+    {
+    public:
+        
+        IndexBuffer(BufferUsage usage, size_t size, void *data=nullptr);
+        ~IndexBuffer();
+    };
+    
+}//end namespace my3d
+
+
+#endif /* defined(__cocos3d_libs__VertexBuffer__) */
